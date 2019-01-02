39046d2160e035e5d3d2f18ec67ed99b00b3dda4
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-02 16:38:23 -0700

    auth index

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from "./providers/AuthProvider"
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
-  <App />, 
+  <AuthProvider>
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>
+  </AuthProvider>,
 document.getElementById('root'));
