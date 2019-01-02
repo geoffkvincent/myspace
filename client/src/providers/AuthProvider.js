7e086631337d182157d7f541292671f487e0004f
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-02 16:26:48 -0700

    authprovider context

--- client/src/providers/AuthProvider.js
+++ client/src/providers/AuthProvider.js
@@ -29,5 +29,15 @@ export class AuthProvider extends React.Component {
     })
   }
 
-  
+  handleLogout = (history) => {
+    axios.delete("/api/auth/sign_out")
+      .then( res => {
+        this.setState({ user: null })
+        history.push('/login')
+      })
+      .catch(res => {
+        console.log(res)
+      }
+    )
+  }
 }
