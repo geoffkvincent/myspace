c7f6e58295bc18922b5a56a23379201843c8285b
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-03 10:53:49 -0700

    login

---
+++ client/src/components/Login.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { AuthConsumer } from "../providers/AuthProvider"
+import { Button, Form, Segment, Header} from 'semantic-ui-react'
+
+class Login extends React.Component {
+  state = { email: '', nickname: '', password: '' }
+
+  render() {
+    const { email, nickname, password } = this.state
+    return (
+      <Segment basic>
+        <Header as='h1' textAlign='center'>Login</Header>
+        <Form>
+          <Form.Input 
+            label="Email"
+            autofocus
+            required
+            name='email'
+            value={email}
+            placeholder='Email'
+            onChange={this.handleChange}
+          />
+          <Form.Input 
+            label="User Name"
+            autofocus
+            required
+            name='nickname'
+            value={nickname}
+            placeholder='User Name'
+            onChange={this.handleChange}
+            />
+          <Form.Input 
+            label="Password"
+            autofocus
+            required
+            name='password'
+            value={password}
+            placeholder='Password'
+            onChange={this.handleChange}
+            />
+          <Segment textAlign='center' basic>
+            <Button primary type='submit'>Submit</Button>
+          </Segment>
+        </Form>
+      </Segment>
+    )
+  }
+}
+
+export default Login
