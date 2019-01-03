e0aabd8075306eebbb200e581253c2a1a4a0b2a2
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-03 13:57:12 -0700

    register form

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,20 +1,28 @@
 import React from 'react'
-import { AuthProvider } from '../providers/AuthProvider'
+import { AuthConsumer } from '../providers/AuthProvider'
 import {  Button, Form, Segment, Header} from 'semantic-ui-react'
 
 class Register extends React.Component{
   state = { email: '', nickname: '', password: '', passwordConfirmation: ''}
 
-  handleSubmit = () => {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { email, nickname, password, passwordConfirmation } = this.state
+    const { auth: { handleRegister }, history } = this.props
 
+    if (password === passwordConfirmation) 
+      handleRegister({ email, nickname, password }, history)
+    else 
+      alert('Passwords Dont Match')
   }
 
-  handleChange = () => {
-
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
   }
 
   render() {
-    const { email, nickname, password, passwordConfirmation} = this.state
+    const { email, nickname, password } = this.state
     return (
       <Segment basic>
         <Header as='h1' textAlign='center'>Register</Header>
@@ -51,3 +59,15 @@ class Register extends React.Component{
     )
   }
 }
+
+  export default class ConnectedRegister extends React.Component {
+    render() {
+      return (
+        <AuthConsumer>
+          { auth =>
+            <Register {...this.props} auth={auth} />
+          }
+        </AuthConsumer>
+      )
+    }
+  }
