1e68fc3f875532067e9deb64418a011393b4f37c
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-03 15:36:40 -0700

    fetch user

--- client/src/components/FetchUser.js
+++ client/src/components/FetchUser.js
@@ -6,7 +6,39 @@ class FetchUser extends React.Component{
   state = { loaded: false }
 
   componentDidMount() {
+    const { auth: { authenticated, handleLogin}} = this.props
 
+    if (authenticated) {
+      this.loaded()
+    } else {
+      if (this.checkLocalToken()) {
+        axios.get('/api/auth/validate_token')
+          .then( res => {
+            handleLogin(res.data.data)
+            this.loaded()
+          })
+          .catch( res => {
+            this.loaded()
+          })
+      } else {
+        this.loaded()
+      }
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!this.state.loaded) this.loaded()
+  }
+
+  checkLocalToken = () => {
+    const token = localStorage.getItem('access-token')
+    return token
+  }
+
+  loaded = () => this.setState({ loaded: true })
+
+  render() {
+    return this.state.loaded ? this.props.children : null
   }
 }
 
