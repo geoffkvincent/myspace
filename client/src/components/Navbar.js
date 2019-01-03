c6a111a5968a751fec971ed525b3b30af4e93164
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-03 09:43:12 -0700

    navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -19,7 +19,22 @@ class Navbar extends React.Component {
       )
     } else {
       return (
-        
+        <Menu.Menu position='right'>
+          <Link to='/login'>
+            <Menu.Item 
+              id='login'
+              name='login'
+              active={location.pathname === '/login'}
+            />
+          </Link>
+          <Link to='register'>
+            <Menu.Item 
+              id='register'
+              name='register'
+              active={location.pathname === '/register'}
+            />
+          </Link>
+        </Menu.Menu>
       )
     }
   }
