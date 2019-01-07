c6af60bd3ff9289539674e5f1a5754fac446a91d
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-07 14:39:56 -0700

    post controller crud actions

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,16 +1,43 @@
 class Api::PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
   def index
+    render json: current_user.posts.all
   end
 
   def show
+    render json: @post
   end
 
   def create
+    post = current_user.post,new(post_params)
+
+    if post.save
+      render json: post
+    else
+      render json: post.error, status 422
+    end
   end
 
   def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.error, status 422
+    end
   end
 
   def destroy
+    @post.destroy
+  end
+
+  private
+
+  def set_post
+    @post = current_user.post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
