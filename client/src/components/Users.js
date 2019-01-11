92c88a6532020dfa5776075c6febb80a4ebbcd17
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-11 13:27:15 -0700

    posts render

---
+++ client/src/components/Users.js
@@ -0,0 +1,25 @@
+import react from 'React'
+import {AuthConsumer} from '../providers/AuthProvider'
+
+class Users extends React.Component{
+
+  
+  render(){
+    return(
+      
+    )
+  }
+}
+
+export default class ConnectedUsers extends React.Component{
+  render() {
+    return (
+      <AuthConsumer>
+        {auth =>
+          <Users { ...this.props } auth={auth}/>
+        }
+      </AuthConsumer>
+    )
+  }
+}
+
