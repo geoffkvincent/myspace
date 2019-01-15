4f001050b91f3436f57f5565f6f3eb054db23879
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-15 11:23:29 -0700

    post create

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -3,8 +3,7 @@ class Api::PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
 
   def index
-      render json: @user.posts.all
-      
+      render json: @user.posts.order(created_at: :desc)
   end
 
   def show
@@ -12,7 +11,7 @@ class Api::PostsController < ApplicationController
   end
 
   def create
-    post = current_user.post.new(post_params)
+    post = current_user.posts.new(post_params)
 
     if post.save
       render json: post
