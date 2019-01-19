c5aff416ffdeb40914ba209aafa4dd5659b3dbc8
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-19 13:36:33 -0700

    render post

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -17,9 +17,9 @@ class Posts extends React.Component{
     this.renderPost()
   }
 
-  renderPost = () =>  (
-  <PostShow />
-  ) 
+  // renderPost = () =>  (
+  // <PostShow />
+  // ) 
   
   // renderPost = () => {
   //   const {posts: {posts}} = this.props
@@ -44,7 +44,7 @@ class Posts extends React.Component{
           <Card.Content>
           <Card.Header onClick={() => this.showPost(post.id)}>{post.title}</Card.Header>
           <p>posted: {moment(post.created_at).format("MMM Do YYYY")}</p>
-          {/* { this.state.showPost ? <PostShow showPost={this.state.post}/> : null } */}
+          { this.state.showPost ? <PostShow showPost={this.state.post}/> : null }
           </Card.Content>
           <Card.Content extra style={{display: 'flex', justifyContent: 'flex-end' }} >
             <Icon onClick={() => this.props.posts.deletePost({id: post.id, userId: post.user_id} ) } name='trash'/>
