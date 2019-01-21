1ba4ecd4e8bdd394af789e94511dbc851260b2bd
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-21 14:01:09 -0700

    profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import {AuthConsumer} from '../providers/AuthProvider'
 import { Card, Header, Container, Image } from 'semantic-ui-react'
+import moment from 'moment'
 
 class Profile extends React.Component {
-    render() {
-      const { auth: { user } } = this.props
+    state = { editing: false }
+
+    toggleEdit = () => {
+      this.setState({ editing: !this.state.editing })
+    }
+    
+    profileView = () => {
       return (
-        <Container>
+        <>
           <Header as='h1' >Profile</Header>
           <Card centered>
             <Image></Image>
             <Card.Content>
               <Card.Header>{user.nickname}</Card.Header>
               <Card.Meta>{user.email}</Card.Meta>
+              <Card.Meta>Member since: {moment(user.created_at).format("MMM Do YYYY")}</Card.Meta>
             </Card.Content>
           </Card>
+        </>
+      )
+    }
+
+    editView = () => {
+
+    }
+
+    render() {
+      const { auth: { user } } = this.props
+      return (
+        <Container>
         </Container>
       )
     }
