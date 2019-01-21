7bf68a60df683c85af556371b75ce86aee0a7259
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-21 13:52:21 -0700

    profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import {AuthConsumer} from '../providers/AuthProvider'
+import { Card, Header, Container, Image } from 'semantic-ui-react'
 
 class Profile extends React.Component {
     render() {
+      const { auth: { user } } = this.props
       return (
-        null
+        <Container>
+          <Header as='h1' >Profile</Header>
+          <Card centered>
+            <Image></Image>
+            <Card.Content>
+              <Card.Header>{user.nickname}</Card.Header>
+              <Card.Meta>{user.email}</Card.Meta>
+            </Card.Content>
+          </Card>
+        </Container>
       )
     }
 }
