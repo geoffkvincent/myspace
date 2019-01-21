d763e42c90f9f1d94c641b7269f86eda1d037d4d
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-21 13:39:43 -0700

    profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import {AuthConsumer} from '../providers/AuthProvider'
 
 class Profile extends React.Component {
-
+    render() {
+      return (
+        null
+      )
+    }
 }
 
-export default Profile
+export default class ConnectedProfile extends React.Component {
+    render() {
+      return (
+        <AuthConsumer>
+          {auth =>
+            <Profile {...this.props} auth={auth}/>
+          }
+        </AuthConsumer>
+      )
+    }
+}
