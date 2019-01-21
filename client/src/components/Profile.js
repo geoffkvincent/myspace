a582d5b5067c146eb1e686ba6be6f4dd371a9097
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-21 14:25:07 -0700

    profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import {AuthConsumer} from '../providers/AuthProvider'
-import { Card, Header, Container, Image } from 'semantic-ui-react'
+import { Card, Header, Container, Image, Form, Button } from 'semantic-ui-react'
 import moment from 'moment'
 
 class Profile extends React.Component {
-    state = { editing: false }
+    state = { editing: false, formValues: { name: '', email: ''} }
 
     toggleEdit = () => {
       this.setState({ editing: !this.state.editing })
     }
     
     profileView = () => {
+      const { auth: { user } } = this.props
       return (
         <>
           <Header as='h1' >Profile</Header>
@@ -27,7 +28,27 @@ class Profile extends React.Component {
     }
 
     editView = () => {
-
+      const { auth: { user } } = this.props
+      const { formValues: { name, email } } = this.state
+      return (
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Input
+            label="Name"
+            name="name"
+            value={name}
+            required
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            label="Email"
+            name="email"
+            value={email}
+            required
+            onChange={this.handleChange}
+          />
+          <Button>Update</Button>
+        </Form>
+      )
     }
 
     render() {
