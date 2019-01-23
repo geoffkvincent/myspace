fb74aeed93c3d01c0bdac3d36c612f0cefd21e92
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-23 14:37:18 -0700

    update user profile

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -22,7 +22,6 @@ class Api::UsersController < ApplicationController
         ext = File.extname(file.tempfile)
         cloud_image = Cloudinary::Uploader.upload(file, public_id: file.original_filename, secure: true)
         user.image = cloud_image['secure_url']
-        binding.pry
       rescue => e
         render json: { errors: e }, status: 422
       end
@@ -42,28 +41,6 @@ class Api::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:friends, :file)
+    params.require(:user).permit(:friends)
   end
 end
-
-# user = User.find(params[:id])
-# user.name = params[:name] ? params[:nickname] : user.nickname
-# user.email = params[:email] ? params[:email] : user.email
-
-# file = params[:file]
-
-# if file
-#   begin
-#     ext = File.extname(file.tempfile)
-#     cloud_image = Cloudinary::Uploader.upload(file, public_id: file.original_filename, secure: true)
-#     user.image = cloud_image['secure_url']
-#   rescue => e
-#     render json: { errors: e }, status: 422
-#   end
-# end
-
-# if user.save
-#   render json: user
-# else
-#   render json: { errors: user.errors.full_messages }, status: 422
-# end
