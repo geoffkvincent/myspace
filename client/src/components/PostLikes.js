ce1a757af7caa839df8aef1bf411c94afacd6457
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-25 15:31:57 -0700

    new route for post likes

--- client/src/components/PostLikes.js
+++ client/src/components/PostLikes.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Table } from 'semantic-ui-react'
 
 class PostLikes extends React.Component{
   render() {
     return (
-      null
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>User</Table.HeaderCell>
+            <Table.HeaderCell>Created at</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+      </Table>
     )
   }
 }
