61cc1fae60b1e5d5788bf08cc3e1d00b0b3bfd7c
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-27 16:35:58 -0700

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,22 @@ user_arr = [
   {email: "test@test3.com", password: "password", nickname: "test3"},
   {email: "test@test4.com", password: "password", nickname: "test4"},
   {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-  {email: "test@test5.com", password: "password", nickname: "test5"}
-   
+  {email: "test@test6.com", password: "password", nickname: "test6"}
+  {email: "test@test7.com", password: "password", nickname: "test7"}
+  {email: "test@test8.com", password: "password", nickname: "test8"}
+  {email: "test@test9.com", password: "password", nickname: "test9"}
+  {email: "test@test10.com", password: "password", nickname: "test10"}
+  {email: "test@test11.com", password: "password", nickname: "test11"}
+  {email: "test@test12.com", password: "password", nickname: "test12"}
+  {email: "test@test13.com", password: "password", nickname: "test13"}
+  {email: "test@test14.com", password: "password", nickname: "test14"}
+  {email: "test@test15.com", password: "password", nickname: "test15"}
+  {email: "test@test16.com", password: "password", nickname: "test16"}
+  {email: "test@test17.com", password: "password", nickname: "test17"}
+  {email: "test@test18.com", password: "password", nickname: "test18"}
+  {email: "test@test19.com", password: "password", nickname: "test19"}
+  {email: "test@test20.com", password: "password", nickname: "test20"}
+
 ]
 
 user_arr.each do |user|
