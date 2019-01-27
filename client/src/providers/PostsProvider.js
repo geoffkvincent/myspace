47d32a771ef69df74bed9f8a87a7bceb2b803796
Geoff Vincent <geoffkvincent@gmail.com>
2019-01-27 11:23:58 -0700

    custom route for update likes

--- client/src/providers/PostsProvider.js
+++ client/src/providers/PostsProvider.js
@@ -21,7 +21,7 @@ export class PostsProvider extends React.Component {
 
   addLike = (post, user) => {
     debugger
-    axios.put(`/api/users/${post.userId}/posts/${post.postId}`, {user})
+    axios.put(`/api/users/${post.userId}/posts/${post.postId}/add_like`, {user})
       .then( ({data}) => {
         const posts = this.state.posts.map( p => {
           if (p.id === post.postId)
